ebbccf007fc8ecb68ad1e45bec4c4d92ae91e378
amarton <amarton@umd.edu>
2021-10-11 12:42:32 -0400

    javascript update

---
+++ javascript-intro/basics/javascript.js
@@ -0,0 +1,24 @@
+// this is a comment
+
+/*
+this is a 
+multiline comment
+*/
+
+
+alert ("Hello, world");
+
+
+
+
+// conditional statement
+
+var amount = 5000;
+
+if ( amount <1000 ) {
+	alert("it's LESS than 1000");
+
+} else {
+	alert("it's MORE than 1000");
+}
+
---
+++ javascript-intro/dynamic-page/javascript.js
@@ -0,0 +1,25 @@
+
+//Define variables
+var headlineArray = ["Next headline", "Another Headline" ,"Yet Another" , "Final Headline"];
+var headlineIndex = 0;
+var headlineLength = headlineArray.length;
+var myLoop = setInterval(changeHeadline,1000);
+
+
+//Function to loop through the headline array
+function changeHeadline () {
+  var headlineElement = document.getElementById("headline");
+   var newHeadline = headlineArray[headlineIndex];
+    //alert(newHeadline);
+  headlineElement.innerHTML = newHeadline;
+    headlineIndex++;
+    	if ( headlineIndex >= headlineLength ) {
+	clearInterval(myLoop);
+	} 
+}
+
+
+//run the function when the page loads
+window.onload = function() {
+  myLoop
+};
